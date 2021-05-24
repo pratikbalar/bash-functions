80898b1db73f612f354f39bc4b2c3ed6ed002317
Pratik <68642400+pratikbalar@users.noreply.github.com>
2021-05-24 15:46:48 +0530

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,9 +1,13 @@
-function tarr(){
+## tarr https://github.com/tcnksm/ghr/releases/download/v0.13.0/ghr_v0.13.0_linux_amd64.tar.gz ghr_v0.13.0_linux_amd64/ghr /usr/bin/ghr
+
+function tarr() {
   TMP_XCURRENT=$(pwd)
-  mv $1 /tmp
-  cd /tmp
-  tar xvzf $1
+  mkdir -p /tmp/tmp_ext
+  cd /tmp/tmp_ext
+  wget -nv $1 -O tmp.tar.gz
+  tar xvzf tmp.tar.gz
   cp $2 $3
   chmod a+x $3
   cd $TMP_XCURRENT
+  rm -rf /tmp/tmp_ext
 }
